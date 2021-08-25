a651c89c32f2ec3e43cd6b6f605fd8eea132c2e0
RKiri <535819695@qq.com>
2021-08-26 03:32:29 +0800

    Client 改造Client 暴露调用接口 Client类保存并初始化Channel 封装Client.send(String msg)函数 client端接收到channelRead后 更新界面
    ClientFrame
    把ClientFrame做成单例
    保存Client中Channel对象，连接完毕之后进行初始化
    窗口显示后调用connect连接
    处理actionPerformed事件时调用send发送
    添加send(String msg)函数
    用初始化好的channel进行传输
    在main中拿到单例对象 显示
    ClientHandler接收到数据后更新frame中的ta

--- src/src/com/weiyuze/nettystudy/s02/ClientFrame.java
+++ src/src/com/weiyuze/nettystudy/s02/ClientFrame.java
@@ -5,8 +5,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ClientFrame extends Frame {
+
+    //把ClientFrame做成单例
+    //一个Client窗口只需要有一个
+    public static final ClientFrame INSTANCE = new ClientFrame();
     TextArea ta = new TextArea();//多行文本
     TextField tf = new TextField();//单行文本
+
+    //保存Client中Channel对象，连接完毕之后进行初始化 1
+    //窗口显示后调用connect连接，
+    Client c = null;
 
     public ClientFrame() {
         this.setSize(600, 400);//大小
@@ -17,14 +25,39 @@ public class ClientFrame extends Frame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //把字符串发送到服务器 接收后对所有客户端转发一遍
-                ta.setText(ta.getText() + tf.getText());
+
+                //处理actionPerformed事件时调用send发送
+                //添加send(String msg)函数
+                //用初始化好的channel进行传输
+                c.send(tf.getText());
+                //ta.setText(ta.getText() + tf.getText());
                 tf.setText("");
             }
         });
-        this.setVisible(true);
+        //this.setVisible(true);
+
+        //窗口显示完毕后调用
+        //new Client().connect();
+
+        //保存Client中Channel对象，连接完毕之后进行初始化 2
+        //connectToServer();
+    }
+
+    void connectToServer() {
+        c = new Client();
+        c.connect();
     }
 
     public static void main(String[] args) {
-        new ClientFrame();
+        //在main中拿到单例对象 显示
+        ClientFrame frame = ClientFrame.INSTANCE;
+        frame.setVisible(true);
+        frame.connectToServer();
+        //new ClientFrame();
+    }
+
+    //ClientHandler接收到数据后更新frame中的ta
+    public void updateText(String msgAccepted) {
+        ta.setText(ta.getText() + System.getProperty("line.separator") + msgAccepted);//System.getProperty()拿到属性的值；win: \r\n 换行回车 ;Linux \n 换行 换行符每种系统不一样 跨系统可这样写
     }
 }
---
+++ src/src/com/weiyuze/nettystudy/s02/Server.java
@@ -0,0 +1,65 @@
+package com.weiyuze.nettystudy.s02;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+public class Server {
+    public static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workGroup = new NioEventLoopGroup(2);
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            ChannelFuture f = b.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new ServerChildHandler());
+                        }
+                    })
+                    .bind(8888)
+                    .sync();
+
+            System.out.println("Server started!");
+
+            f.channel().closeFuture().sync();
+        } finally {
+            workGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+
+}
+
+class ServerChildHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Server.clients.add(ctx.channel());
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] bytes = new byte[buf.readableBytes()];
+        buf.getBytes(buf.readerIndex(), bytes);
+        System.out.println(new String(bytes));
+        Server.clients.writeAndFlush(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
